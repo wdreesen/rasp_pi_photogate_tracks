9517b125012f33b0061bf10b8545b7657d152f7f
wdreesen <38812694+wdreesen@users.noreply.github.com>
2020-02-29 14:17:31 -0700

    Update read_gpio_input_pin.py

--- read_gpio_input_pin.py
+++ read_gpio_input_pin.py
@@ -1,13 +1,14 @@
 import sys
 import RPi.GPIO as GPIO
 
+# Setup GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(<pin#>,GPIO.IN) # Test Pin
 
-try:
-    while True:
+while True:
+    try:
         print(‘Start:',GPIO.input(<pin#>))
        
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    sys.exit()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
